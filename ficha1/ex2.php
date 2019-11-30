0a04eb626c955ec3222fffe30506ce122cab086f
Rhazar96 <58366666+Rhazar96@users.noreply.github.com>
2019-11-30 16:43:37 +0000

    Create ex2.php

---
+++ ficha1/ex2.php
@@ -0,0 +1,33 @@
+<?php
+       /************* DESCRIÇÃO DO DOCUMENTO ************
+        * Nome do ficheiro: ex2.php                     *
+        * UC: P2                                        *
+        * @author Cláudio Ferreira                      *
+        * @version 1.0                                  *
+        * Data: Novembro de 2019                        *
+        * Descrição: exercício prático                  *
+        *************************************************/
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-pt">
+
+<head>
+<meta charset="utf-8">
+<title> Exercícios de P2 - Cláudio Ferreira </title>
+<link rel="stylesheet" href="css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<div class=header> </div>
+
+<p> 
+
+</body>
+
+
+
+</html>

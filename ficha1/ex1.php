33d71bde5d99c0999c4fccf4951004b24fb0dfe3
Rhazar96 <58366666+Rhazar96@users.noreply.github.com>
2019-11-30 16:00:19 +0000

    Update ex1.php

--- ficha1/ex1.php
+++ ficha1/ex1.php
@@ -1,5 +1,14 @@
 <?php
+       /************* DESCRIÇÃO DO DOCUMENTO ************
+        * Nome do ficheiro: ex1.php                     *
+        * UC: P2                                        *
+        * @author Cláudio Ferreira                      *
+        * @version 1.0                                  *
+        * Data: JNovembro de 2019                       *
+        * Descrição: exercício prático                  *
+        *************************************************/
 
 phpinfo()
+
 
 ?>
